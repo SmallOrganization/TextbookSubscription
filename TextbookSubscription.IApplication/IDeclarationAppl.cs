cb3e10204ea5b941d1d3e7dc2f118d1ce9a059a3
Zhang TongXie <mikcczhang@qq.com>
2018-09-02 14:45:07 +0800

    调整接口

---
+++ TextbookSubscription.IApplication/IDeclarationAppl.cs
@@ -0,0 +1,11 @@
+﻿namespace TextbookSubscription.IApplication
+{
+    public interface IDeclarationAppl
+    {
+        /// <summary>
+        /// 通过excel导入申报计划
+        /// </summary>
+        /// <param name="excelPath">excel路径</param>
+        void Import(string excelPath);
+    }
+}
